25ba920b7b3a8073275d855a8c942a8b68dd16e9
Vaishnavi C P <vaishnavicp@Vaibhavs-MacBook-Air.local>
2026-05-30 21:24:10 +0530

    Show project fetch error

--- backend/src/routes/projectRoutes.js
+++ backend/src/routes/projectRoutes.js
@@ -17,8 +17,8 @@ router.get("/", async (req, res) => {
     console.error(error)
 
     res.status(500).json({
-      message: "Failed to fetch projects"
-    })
+  message: error.message
+})
   }
 })
 
